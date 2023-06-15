e5101f7f9a803f781c356441b2441c711d4aaed2
HunterGooD <vlad.gudov@inbox.ru>
2023-06-16 00:43:09 +0400

    init game setting

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 run:
 	# cargo dylint bevy_lint
-	cargo run --features bevy/dynamic_linking
-	# cargo run --features bevy/dynamic
+	cargo run --features bevy/dynamic_linking # bevy 10.0
+	# cargo run --features bevy/dynamic # bevy 9.0
 
 build_win:
 	cargo build --target=x86_64-pc-windows-gnu --release
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use bevy::{
+    prelude::*,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    window::{PresentMode, WindowMode}
+};
+use bevy_rapier2d::prelude::*;
+use leafwing_input_manager::prelude::*;
+use leafwing_input_manager::InputManagerBundle;
+
 fn main() {
-    println!("Hello, world!");
+    // create App main struct for run game in bevy 
+    App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "It's a game!".into(),
+            resolution: (1920., 1080.).into(),
+            resizable: false,
+            present_mode: PresentMode::AutoVsync,
+            mode: WindowMode::Fullscreen,
+            ..default()
+        }),
+        ..default()
+    })) 
+    .add_plugin(LogDiagnosticsPlugin::default())
+    .add_plugin(FrameTimeDiagnosticsPlugin)
+    .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0)) // create world rapier physics
+    .insert_resource(RapierConfiguration {
+        gravity: Vec2::new(0.0, -2000.0),
+        ..default()
+    }) 
+    .add_plugin(RapierDebugRenderPlugin::default())
+    .add_plugin(InputManagerPlugin::<PlayerActions>::default()) // player actions for buttons 
+    .add_system(setup_graphics)
+    .run();
+}
+
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
+enum PlayerActions {
+    Move, 
+    Jump 
+}
+
+#[derive(Clone, Default, Debug, Component)]
+struct Name(String);
+
+#[derive(Clone, Default, Debug, Component)]
+struct Player;
+
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
+    commands.spawn(Camera2dBundle::default());
 }
